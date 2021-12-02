4c7d681539804b2c07f7ceda4bfa7bc23eff28aa
Chauncey Deone Hester Jr <chauncey@debian.attlocal.net>
2021-12-01 18:49:05 -0600

    commented out

--- SpiderBot/SensorReading/src/main.c
+++ SpiderBot/SensorReading/src/main.c
@@ -13,27 +13,29 @@ int main(){
     //set_up_I2C()
     
     
-    ledHandler = metal_gpio_get_device(0);
+    // ledHandler = metal_gpio_get_device(0);
 
-    led[0] = 19;
-    led[1] = 21;
-    led[2] = 22;
-    for(int i = 0; i < 3; i++){
-        metal_gpio_set_pin(ledHandler,led[i],1);
-        metal_gpio_enable_output(ledHandler,led[i]);
-        delay(1000);
-    }
+    // led[0] = 19;
+    // led[1] = 21;
+    // led[2] = 22;
+    // for(int i = 0; i < 3; i++){
+    //     metal_gpio_set_pin(ledHandler,led[i],1);
+    //     metal_gpio_enable_output(ledHandler,led[i]);
+    //     delay(1000);
+    // }
     
-    for(int i = 0; i < 3; i++){
-        metal_gpio_set_pin(ledHandler,led[i],0);
-        delay(500);
-        metal_gpio_set_pin(ledHandler,led[i],1);
-        delay(500);
-    }
-    for(int i = 0; i < 3; i++){
-        metal_gpio_disable_output(ledHandler,led[i]);
-        delay(1000);
-    }
+    // for(int i = 0; i < 3; i++){
+    //     metal_gpio_set_pin(ledHandler,led[i],0);
+    //     delay(500);
+    //     metal_gpio_set_pin(ledHandler,led[i],1);
+    //     delay(500);
+    // }
+    // for(int i = 0; i < 3; i++){
+    //     metal_gpio_disable_output(ledHandler,led[i]);
+    //     delay(1000);
+    // }
+
+    
 
     
     
